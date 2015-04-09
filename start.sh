d1e246ad89efcfb2eaab9121ad343bd5a990b6af
Fabian Ström <fs@fabianstrom.se>
2015-04-09 22:25:56 +0200

    removed stopping pdns

--- start.sh
+++ start.sh
@@ -5,6 +5,5 @@ service postgresql start
 service apache2 start
 su - postgres -c '/usr/lib/postgresql/9.3/bin/createdb dns'
 su - postgres -c 'psql dns < /schema.sql'
-service pdns stop
 
 /bin/bash
