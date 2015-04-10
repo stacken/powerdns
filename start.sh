9836d360c0016c80d41a7a3cf18fde9bd407370c
Fabian Ström <fs@fabianstrom.se>
2015-04-10 18:49:09 +0200

    moved db creation to build. removed apache

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 
 service pdns start
 service postgresql start
-service apache2 start
-su - postgres -c '/usr/lib/postgresql/9.3/bin/createdb dns'
-su - postgres -c 'psql dns < /schema.sql'
 
 /bin/bash
